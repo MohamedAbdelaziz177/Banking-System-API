2f3f1cf4a4d4d761c5ce5c9b1070ad1cab92fd26
Mohamed Abdelaziz <145907767+MohamedAbdelaziz177@users.noreply.github.com>
2025-03-23 17:58:20 +0200

    Delete Repositories directory

--- Repositories/IRepositories/ICustomerRepo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Banking_system.Model;
-
-namespace Banking_system.Repositories.IRepositories
-{
-    public interface ICustomerRepo : IGenericRepo<Customer>
-    {
-        
-    }
-}
--- Repositories/IRepositories/IRefreshTokenRepo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Banking_system.Model;
-
-namespace Banking_system.Repositories.IRepositories
-{
-    public interface IRefreshTokenRepo : IGenericRepo<RefreshToken>
-    {
-        Task<RefreshToken?> GetValidRefreshTokenAsync(string refreshToken);
-    }
-}
